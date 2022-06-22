9ecef0b76b80eab359fd56e61e9f5ca3b4926332
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-06-21 19:38:55 -0600

    get dict of all file types in letters

--- demand_letter_analysis.py
+++ demand_letter_analysis.py
@@ -5,6 +5,8 @@ import docx
 
 list_of_demand_letters = [] # total list of all letters and their locations
 
+## PRE-PROCESSING RAW DIRECTORIES AND FILES ############################
+
 def GetListOfLettersFromDirectory(path_directory):
 	# get all the letters from a directory
 	print("Directories = {0}".format(path_directory))
@@ -15,24 +17,27 @@ def GetListOfLettersFromDirectory(path_directory):
 			else:
 				list_of_demand_letters.append(os.path.join(path_directory, file_name))
 
-if __name__ == '__main__':
-	# reference to all demand letters stored in demand_letters/
-	root_directory = "demand_letters"
-	GetListOfLettersFromDirectory(root_directory)
-	print("Total Letters = {0}".format(len(list_of_demand_letters)))
-
+def GetFileTypes():
 	# find types of files
-	'''
 	extension_types_and_instances_dict = {}
 	for letter in list_of_demand_letters:
 		#print(letter)
-		print(letter.split("."))
+		#print(letter.split("."))
 		#print(letter.split(".")[1])
-		extension = letter.split(".")[1]
+		extension = letter.split(".")[-1]
 		if extension not in extension_types_and_instances_dict.keys():
 			extension_types_and_instances_dict[extension] = 1
 		else:
 			extension_types_and_instances_dict[extension] += 1
-	print(extension_types_and_instances_dict)
-	print(list_of_demand_letters[0])
-	'''
+	return extension_types_and_instances_dict
+
+########################################################################
+
+if __name__ == '__main__':
+	# reference to all demand letters stored in demand_letters/
+	root_directory = "demand_letters"
+	GetListOfLettersFromDirectory(root_directory)
+	print("Total Letters = {0}\n".format(len(list_of_demand_letters)))
+
+	extension_dict = GetFileTypes()
+	print("File types: {0}\n".format(extension_dict))
